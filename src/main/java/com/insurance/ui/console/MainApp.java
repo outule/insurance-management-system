7d6eafbfc49204730b59e585aafafd1903245d63
Outule <cse20-049>
2026-04-24 15:30:28 +0200

    chore: initialize project structure and main entry point

---
+++ src/main/java/com/insurance/ui/console/MainApp.java
@@ -0,0 +1,9 @@
+package com.insurance.ui.console;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Insurance Management System Started...");
+    }
+
+}
